b93ce2a289db39568adb76004d0345e96d20dcb1
Valentina Nikitenko <d4rk.hela@gmail.com>
2024-11-16 08:56:53 +0200

    Add FAQ page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/tpass/'
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        faq: resolve(__dirname, 'faq.html'),
+      }
+    }
+  }
 });
