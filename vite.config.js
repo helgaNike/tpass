3a8dca90867742944927de3e2d67fa597e70fac5
Valentina Nikitenko <d4rk.hela@gmail.com>
2024-11-16 09:36:39 +0200

    Vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-const pages = {
-  index: resolve(__dirname, 'index.html'),
-  faq: resolve(__dirname, 'faq.html'),
-};
-
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? './' : '/tpass/',
-  build: {
-    rollupOptions: {
-      input: pages
+export default defineConfig({
+    base: '/tpass/',
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, "index.html"),
+                faq: resolve(__dirname, "faq.html"),
+            }
+        }
     }
-  }
-}));
+
+});
