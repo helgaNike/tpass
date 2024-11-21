5b5705ab7a5b6a51b3faa8f8b233567d04a464bd
Valentina Nikitenko <d4rk.hela@gmail.com>
2024-11-21 10:18:23 +0200

    get app script refactor

--- js/send-link-form.js
+++ js/send-link-form.js
@@ -1,38 +1,56 @@
 import IMask from 'imask';
 
-const sendLinkForm = document.querySelector('.get-app-form');
-const phoneInput = sendLinkForm.querySelector('.get-app-form__input');
-const submitBtn = sendLinkForm.querySelector('.get-app-form__btn');
-const successMessage = document.querySelector('.get-app__success-msg');
-const okBtn = successMessage.querySelector('.get-app__success-msg-btn');
+class GetAppBlock {
+    constructor(appBlock) {
+        this.appBlock = appBlock;
+        this.sendLinkForm = this.appBlock.querySelector('.get-app-form');
+        this.phoneInput = this.sendLinkForm.querySelector('.get-app-form__input');
+        this.submitBtn = this.sendLinkForm.querySelector('.get-app-form__btn');
+        this.successMessage = this.appBlock.querySelector('.get-app__success-msg');
+        this.okBtn = this.successMessage.querySelector('.get-app__success-msg-btn');
 
-const maskOptions = {
-    mask: '+{1}(000)000-0000',
-  };
+        this.maskOptions = {
+            mask: '+{1}(000)000-0000',
+        };
+        this.mask = IMask(this.phoneInput, this.maskOptions);
 
-const mask = IMask(phoneInput, maskOptions);
-
-submitBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    if(!phoneInput.value) {
-        phoneInput.classList.add('error');
+        this.addEvents();
     }
-    else {
-        phoneInput.classList.remove('error');
-        this.disabled = true;
-        const submit = new Promise(function(resolve, reject){
-            setTimeout(()=>resolve("done"), 1000);
-        });
-        submit.then(result => {
-            this.disabled = false;
-            sendLinkForm.classList.add('hidden');
-            successMessage.classList.remove('hidden');    
+    isFormValidate() {
+        if (!this.phoneInput.value) {
+            this.phoneInput.classList.add('error');
+            return false;
+        }
+        this.phoneInput.classList.remove('error'); 
+        return true;
+    }
+    submitForm(e) {
+        e.preventDefault();
+        if(this.isFormValidate()) {
+            this.submitBtn.disabled = true;
+            const submit = new Promise(function (resolve, reject) {
+                setTimeout(() => resolve("done"), 1000);
+            });
+            submit.then(result => {
+                this.submitBtn.disabled = false;
+                this.sendLinkForm.classList.add('hidden');
+                this.successMessage.classList.remove('hidden');
+            });
+        }
+    }
+    addEvents() {
+        this.submitBtn.addEventListener('click', this.submitForm.bind(this));
+        this.okBtn.addEventListener('click', () => {
+            this.sendLinkForm.classList.remove('hidden');
+            this.successMessage.classList.add('hidden');
+            this.mask.value = '';
         });
     }
-}); 
 
-okBtn.addEventListener( 'click', function(){
-    sendLinkForm.classList.remove('hidden');
-    successMessage.classList.add('hidden');
-    mask.value = '';
-});
+}
+
+const getAppBlock = document.querySelector('.get-app');
+
+if (getAppBlock) {
+    new GetAppBlock(getAppBlock);
+}
